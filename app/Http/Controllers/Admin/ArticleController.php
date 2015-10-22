bf9cb5095e8e588c9a06127325e14c08cddc36fc
Qingcai <1016903103@qq.com>
2015-10-22 14:10:49 +0800

    format

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -8,119 +8,119 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller {
 
-	
-	private $preView = 'admin.article.';
-	
-	/**
-	 * Get Name of View
-	 *
-	 * @return ViewName
-	 */
-	public function getView($name) {
-		return $this->preView . $name;
-	}
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index() {
-		return View::make($this->getView('index'))->withArticles(Article::all());
-	}
+    private $preView = 'admin.article.';
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create() {
-		return View::make($this->getView('create'));
-	}
+    /**
+     * Get Name of View
+     *
+     * @return ViewName
+     */
+    public function getView($name) {
+        return $this->preView . $name;
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(Request $request) {
-		$validator = Validator::make($request->all(),[	
-			'title' => 'required|max:40',
-			'subtitle' => 'required|max:80',
-			'summary' => 'required|max:300',
-			'content' => 'required',
-			'weight' => 'required|integer',
-			'allow_comment' => 'boolean',
-		]);
-		if ($validator->fails()) {
-		    return Redirect::back()->withErrors($validator->errors());
-		}
-		if ($article = Article::create($request->all())) {
-			return View::make($this->getView('edit'))->with('success','发布成功！')->withArticle($article);
-		} else {
-			return View::make($this->getView('create'))->with('error', '发布失败');
-		}
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        return View::make($this->getView('index'))->withArticles(Article::all());
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id) {
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        return View::make($this->getView('create'));
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id) {
-		return View::make($this->getView('edit'))->withArticle(Article::find($id));
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:40',
+            'subtitle' => 'required|max:80',
+            'summary' => 'required|max:300',
+            'content' => 'required',
+            'weight' => 'required|integer',
+            'allow_comment' => 'boolean',
+        ]);
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator->errors());
+        }
+        if ($article = Article::create($request->all())) {
+            return View::make($this->getView('edit'))->with('success', '发布成功！')->withArticle($article);
+        } else {
+            return View::make($this->getView('create'))->with('error', '发布失败');
+        }
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update(Request $request, $id) {
-		$validator = Validator::make($request->all(),[	
-			'title' => 'required|max:40',
-			'subtitle' => 'required|max:80',
-			'summary' => 'required|max:300',
-			'content' => 'required',
-			'weight' => 'required|integer',
-			'allow_comment' => 'boolean',
-		]);
-		if ($validator->fails()) {
-		    return View::make($this->getView('edit'))->withArticle(Article::find($id))->withErrors($validator->errors());
-		}
-		$article = Article::find($id);
-		if ($article->update($request->all())) {
-			return View::make($this->getView('edit'))->with('success','修改成功！')->withArticle(Article::find($id));
-		} else {
-			return View::make($this->getView('edit'))->with('error', '修改失败！')->withArticle(Article::find($id));
-		}
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id) {
-		$article = Article::find($id);
-		if ($article->delete()) {
-			return Redirect::to('admin/article')->with('success','删除成功！');
-		} else {
-			return Redirect::to('admin/article')->with('error', '删除失败！');
-		}
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id) {
+        return View::make($this->getView('edit'))->withArticle(Article::find($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update(Request $request, $id) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:40',
+            'subtitle' => 'required|max:80',
+            'summary' => 'required|max:300',
+            'content' => 'required',
+            'weight' => 'required|integer',
+            'allow_comment' => 'boolean',
+        ]);
+        if ($validator->fails()) {
+            return View::make($this->getView('edit'))->withArticle(Article::find($id))->withErrors($validator->errors());
+        }
+        $article = Article::find($id);
+        if ($article->update($request->all())) {
+            return View::make($this->getView('edit'))->with('success', '修改成功！')->withArticle(Article::find($id));
+        } else {
+            return View::make($this->getView('edit'))->with('error', '修改失败！')->withArticle(Article::find($id));
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id) {
+        $article = Article::find($id);
+        if ($article->delete()) {
+            return Redirect::to('admin/article')->with('success', '删除成功！');
+        } else {
+            return Redirect::to('admin/article')->with('error', '删除失败！');
+        }
+    }
 
 }
--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -9,115 +9,115 @@ use Redirect, Input, Auth, Validator, View;
 class TagController extends Controller {
 
 
-	private $preView = 'admin.tag.';
-	
-	/**
-	 * Get Name of View
-	 *
-	 * @return ViewName
-	 */
-	public function getView($name) {
-		return $this->preView . $name;
-	}
+    private $preView = 'admin.tag.';
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index() {
-		return View::make($this->getView('index'))->withTags(Tag::paginate(15));
-	}
+    /**
+     * Get Name of View
+     *
+     * @return ViewName
+     */
+    public function getView($name) {
+        return $this->preView . $name;
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create() {
-		return View::make($this->getView('create'))->withTags(Tag::all());
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        return View::make($this->getView('index'))->withTags(Tag::paginate(15));
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(Request $request) {
-		$validator = Validator::make($request->all(),[	
-			'name' => 'required|max:255',
-			'parent' => 'required',
-		]);
-		if ($validator->fails()) {
-		    return Redirect::back()->withErrors($validator->errors());
-		}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        return View::make($this->getView('create'))->withTags(Tag::all());
+    }
 
-		if (Tag::create($request->all())) {
-			return Redirect::to('admin/tag')->with('success','添加成功！');
-		} else {
-			return Redirect::back()->with('error','添加失败！');
-		}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'parent' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator->errors());
+        }
 
-	}
+        if (Tag::create($request->all())) {
+            return Redirect::to('admin/tag')->with('success', '添加成功！');
+        } else {
+            return Redirect::back()->with('error', '添加失败！');
+        }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id) {
-		//
-	}
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id) {
-		return View::make($this->getView('edit'))->with("thisTag", Tag::find($id))->withTags(Tag::all());
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update(Request $request, $id) {
-		$validator = Validator::make($request->all(),[	
-			'name' => 'required|max:255',
-			'parent' => 'required',
-		]);
-		if ($validator->fails()) {
-		    return Redirect::back()->withErrors($validator->errors());
-		}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id) {
+        return View::make($this->getView('edit'))->with("thisTag", Tag::find($id))->withTags(Tag::all());
+    }
 
-		$tag = Tag::find($id);
-		if ($tag->update($request->all())) {
-			return Redirect::to('admin/tag')->with('success','修改成功！');
-		} else {
-			return Redirect::back()->withInput->with('error', '修改失败！');
-		}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update(Request $request, $id) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'parent' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator->errors());
+        }
 
-	}
+        $tag = Tag::find($id);
+        if ($tag->update($request->all())) {
+            return Redirect::to('admin/tag')->with('success', '修改成功！');
+        } else {
+            return Redirect::back()->withInput->with('error', '修改失败！');
+        }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id) {
-		$tag = Tag::find($id);
-		if ($tag->delete()) {
-			return Redirect::to('admin/tag')->with('success','删除成功！');
-		} else {
-			return Redirect::to('admin/tag')->with('error', '删除失败！');
-		}
+    }
 
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id) {
+        $tag = Tag::find($id);
+        if ($tag->delete()) {
+            return Redirect::to('admin/tag')->with('success', '删除成功！');
+        } else {
+            return Redirect::to('admin/tag')->with('error', '删除失败！');
+        }
+
+    }
 
 }
